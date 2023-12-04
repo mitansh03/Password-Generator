64695675ed046206037e58c29a7a9b8a8c96f6c7
Mitansh Sahare <87095117+mitansh03@users.noreply.github.com>
2023-12-05 01:41:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const passwordBox = document.getElementById("password");
 const char = "^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[^da-zA-Z]).{8,15}$";
 const all = char;
 const length = 12;
-submitButton.addEventListener("click", () => {
+submitButton.addEventListener("click", (e) => {
   let password = "";
   
   password += char[Math.floor(Math.random() * char.length)];
