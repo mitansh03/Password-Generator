bf4ab3e06a6515a38b76fb7dfb72497355c148ef
Mitansh Sahare <87095117+mitansh03@users.noreply.github.com>
2023-12-05 01:17:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
 const submitButton = document.getElementById("btn");
 const passwordBox = document.getElementById("password");
 
-/* const alphabets = '/([A-Z][a-z])\w+/g'
-const num = '/([0-9])/g' */
 const char = "^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[^da-zA-Z]).{8,15}$";
 const all = /* alphabets; + num + */ char;
 const length = 12;
 const generate = () => {
   let password = "";
-/*   password += alphabets[Math.floor(Math.random() * alphabets.length)];
-  password += num[Math.floor(Math.random() * num.length)]; */
   password += char[Math.floor(Math.random() * char.length)];
 
   while (length > password.length) {
